d8fe71d93b1f3db9f68a333de91cf71e9daaa49a
LAPTOP-1H4J36OP\Fedor Andreevich <7934650@gmail.com>
2021-08-31 19:44:19 +0300

    Donr AM-2

---
+++ MainProgekt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MainProgekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            Console.WriteLine("Ведите двузначное число");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = a / 10;
+            c = a % 10;
+            if ((b + c) % 3 == 0)
+            {
+                Console.WriteLine("Сумма цифр кратна трем");
+            }
+            else
+            {
+                Console.WriteLine("Сумма цифр не кратна трем");
+            }
+            Console.WriteLine("Ведите число d");
+            d = Convert.ToInt32(Console.ReadLine());
+            if ((b + c) % d == 0)
+            {
+                Console.WriteLine($"Сумма цифр кратна {d}");
+            }
+            else
+            {
+                Console.WriteLine($"Сумма цифр не кратна {d}");
+            }
+        }
+    }
+}
